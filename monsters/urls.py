12ad31b02733f4787e4d05c53987cbb5535d9aea
Fab <fabianjarrett@gmail.com>
2021-12-27 18:58:32 +0000

    Add user monster list

--- monsters/urls.py
+++ monsters/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import details, list_all
+from .views import details, list_all, user_list
 
 
 app_name = 'monsters'
 
 urlpatterns = [
     path('', list_all, name='list_all'),
+    path('<username>/', user_list, name='user_list'),
     path('<username>/<name>/', details, name='details'),
 ]
--- monsters/views.py
+++ monsters/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render, get_object_or_404
 
 from .models import Monster
@@ -11,3 +12,8 @@ def list_all(request):
 def details(request, username, name):
     monster = get_object_or_404(Monster, owner__username=username, name=name)
     return render(request, 'monsters/details.html', {'monster': monster})
+
+
+def user_list(request, username):
+    user = get_object_or_404(get_user_model(), username=username)
+    return render(request, 'monsters/user_list.html', {'user': user})
