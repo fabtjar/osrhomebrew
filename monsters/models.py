9b3e2d14d48277b0933fdefccf31e14d212c20ac
Fab <fabianjarrett@gmail.com>
2022-01-05 21:19:58 +0000

    Add monster ordering

--- monsters/models.py
+++ monsters/models.py
@@ -11,6 +11,11 @@ class Monster(models.Model):
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="monsters"
     )
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("-created_date",)
 
     def get_absolute_url(self):
         return reverse("monsters-monster_detail", kwargs={"monster_id": self.id})
@@ -30,6 +35,10 @@ class SpecialAttack(models.Model):
     monster = models.ForeignKey(
         Monster, on_delete=models.CASCADE, related_name="special_attacks"
     )
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ("created_date",)
 
     def __str__(self):
         return self.name
