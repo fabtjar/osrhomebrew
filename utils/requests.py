f3e9951d15e786cce3c6167027f98d2e498de50f
Fab <fabianjarrett@gmail.com>
2022-01-08 12:35:51 +0000

    Don't show login for referer

--- utils/requests.py
+++ utils/requests.py
@@ -1,8 +1,17 @@
 from urllib.parse import urlparse
 
+from django.conf import settings
+from django.urls import reverse
+
 
 def get_host_referer(request):
     """Return the referer URL if it is of the requested host."""
     referer = request.META.get("HTTP_REFERER")
-    if urlparse(referer).netloc == request.get_host():
+
+    parsed_url = urlparse(referer)
+
+    if parsed_url.path == reverse(settings.LOGIN_URL):
+        return
+
+    if parsed_url.netloc == request.get_host():
         return referer
