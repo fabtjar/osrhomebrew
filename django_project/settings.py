276ba5cf526fb240c1b005c1ddfe41af37e0bad2
Fab <fabianjarrett@gmail.com>
2022-01-10 16:29:14 +0000

    Add environs file

--- django_project/settings.py
+++ django_project/settings.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 
 from django.contrib import messages
+from environs import Env
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
