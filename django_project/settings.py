525b6ad17982b44820ab65cbc20b1953405bdb9e
Fab <fabianjarrett@gmail.com>
2022-01-10 18:56:04 +0000

    Temp debugging

--- django_project/settings.py
+++ django_project/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str("SECRET_KEY")
 
-DEBUG = env.bool("DEBUG", False)
+DEBUG = env.bool("DEBUG", True)
 
 ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
