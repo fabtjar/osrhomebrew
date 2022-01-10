d20a53fdc515dd8ac9b1e7b7b3f246a8933cb973
Fab <fabianjarrett@gmail.com>
2022-01-10 18:50:28 +0000

    Remove comment

--- django_project/settings.py
+++ django_project/settings.py
@@ -6,7 +6,6 @@ from environs import Env
 env = Env()
 env.read_env()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
